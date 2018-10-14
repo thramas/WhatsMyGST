193d5d7199db537cf23127ce02053cc1a9cd0153
samarth <samarthiskhare@gmail.com>
2018-10-14 14:14:08 +0530

    changing theme to no action bar

--- app/src/main/java/com/pukingminion/whatsmygst/MainActivity.java
+++ app/src/main/java/com/pukingminion/whatsmygst/MainActivity.java
@@ -23,6 +23,8 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.CompoundButton;
